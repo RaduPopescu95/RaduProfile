5d629e714e5f10e746f675dfd1ab0561b2cf9aa6
Radu <radu.p1995@yahoo.com>
2021-10-07 22:00:55 +0300

    Testimonials completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Particles from "react-particles-js";
-import Navbar from "./components/Navbar";
-import Header from "./components/Header";
-import AboutMe from './components/AboutMe';
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import AboutMe from './components/aboutMe/AboutMe';
 import Services from './components/services/Services';
 import Experience from './components/experience/Experience';
+import Portofolio from './components/portofolio/Portofolio';
+import Testimonials from './components/testimonials/Testimonials';
 
 function App() {
   return (
@@ -36,6 +38,8 @@ function App() {
       <AboutMe/>
       <Services/>
       <Experience/>
+      <Portofolio/>
+      <Testimonials/>
     </>
   );
 }
